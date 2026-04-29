5164a09d95e47b293a1387fb8e9e6b3579963687
sendebookhere <sendebookhere@gmail.com>
2026-04-28 19:20:21 -0500

    Delete src directory

--- src/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mjumydzjawapsomduwgj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qdW15ZHpqYXdhcHNvbWR1d2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MTA3MDMsImV4cCI6MjA5Mjk4NjcwM30.PBJ0ha5dgkBzoLTXRwPJeaemaO6ME9AZCCIpa3p4BV4'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
